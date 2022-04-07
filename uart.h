9c5ca9e30252844b9f682d281500839d7f03b7d6
kim-ado <ingebobl@stud.ntnu.no>
2022-04-07 09:41:15 +0200

    ny uart.h

--- uart.h
+++ uart.h
@@ -1,49 +1,58 @@
+#ifndef UART_H
+#define UART_H
 #include <stdint.h>
-#include "gpio.h"
 
-#define UART ((NRF_UART_REG*)0x40002000) 
+
+#define UART ((NRF_UART_REG*)0x40002000)
+
+typedef struct {
+	//Tasks
+	volatile uint32_t TASKS_STARTRX;
+	volatile uint32_t TASKS_STOPRX;
+	volatile uint32_t TASKS_STARTTX;
+	volatile uint32_t TASKS_STOPTX;
+	volatile uint32_t RESERVED0[3];
+	volatile uint32_t TASKS_SUSPEND;
+	volatile uint32_t RESERVED1[56];
+
+	//Events
+	volatile uint32_t EVENTS_CTS;
+	volatile uint32_t EVENTS_NCTS;
+	volatile uint32_t EVENTS_RXDRDY;
+	volatile uint32_t RESERVED2[4];
+	volatile uint32_t EVENTS_TXDRDY;
+	volatile uint32_t RESERVED3;
+	volatile uint32_t EVENTS_ERROR;
+	volatile uint32_t RESERVED4[7];
+	volatile uint32_t EVENTS_RXTO;
+	volatile uint32_t RESERVED5[46]; 
+
+	//Registers
+	volatile uint32_t SHORTS;
+	volatile uint32_t RESERVED6[64];
+	volatile uint32_t INTENSET;
+	volatile uint32_t INTENCLR;
+	volatile uint32_t RESERVED7[93];
+	volatile uint32_t ERRORSRC;
+	volatile uint32_t RESERVED8[31];
+	volatile uint32_t ENABLE;
+	volatile uint32_t RESERVED9;
+	volatile uint32_t PSELRTS;
+	volatile uint32_t PSELTXD;
+	volatile uint32_t PSELCTS;
+	volatile uint32_t PSELRXD;
+	volatile uint32_t RXD;
+	volatile uint32_t TXD;
+	volatile uint32_t RESERVED10;
+	volatile uint32_t BAUDRATE;
+	volatile uint32_t RESERVED11[17];
+	volatile uint32_t CONFIG;
+
+} NRF_UART_REG;
 
 void uart_init();
 void uart_send(char letter);
 char uart_read();
+void uart_send_str(char ** str);
 
-typedef struct {
-    volatile uint32_t TASKS_STARTRX;
-    volatile uint32_t TASKS_STOPRX;
-    volatile uint32_t TASKS_STARTTX;
-    volatile uint32_t TASKS_STOPTX;
-    volatile uint32_t RESERVD_1[3];
-    volatile uint32_t TASKS_SUSPEND;
-    volatile uint32_t RESERVD_2[56];
-    volatile uint32_t EVENTS_CTS;
-    volatile uint32_t EVENTS_NCTS;
-    volatile uint32_t EVENTS_RXDRDY;
-    volatile uint32_t RESERVD_3[4];
-    volatile uint32_t EVENTS_TXDRDY;
-    volatile uint32_t RESERVD_4[1];
-    volatile uint32_t EVENTS_ERROR;
-    volatile uint32_t RESERVD_5[7];
-    volatile uint32_t EVENTS_RXTO;
-    volatile uint32_t RESERVD_6[46];
-    volatile uint32_t SHORTS;
-    volatile uint32_t RESERVD_7[64];
-    volatile uint32_t INTENSET;
-    volatile uint32_t INTENCLR;
-    volatile uint32_t RESERVD_8[93];
-    volatile uint32_t ERRORSRC;
-    volatile uint32_t RESERVD_9[31];
-    volatile uint32_t ENABLE;
-    volatile uint32_t RESERVD_10[1];
-    volatile uint32_t PSELRTS;
-    volatile uint32_t PSELRXD;
-    volatile uint32_t PSELCTS;
-    volatile uint32_t PSELTXD;
-    volatile uint32_t RXD;
-    volatile uint32_t TXD;
-    volatile uint32_t RESERVD_11[1];
-    volatile uint32_t BAUDRATE;
-    volatile uint32_t RESERVD_12[17];
-    volatile uint32_t CONFIG;
-}NRF_UART_REG;
-
-
+#endif
